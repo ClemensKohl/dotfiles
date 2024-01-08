0f3f56c8fe6bf34015b1bc7fbc3291539bdbeb5f
Clemens Kohl <kohl.clemens@gmail.com>
2024-01-08 23:19:38 +0100

    tmux back

--- vim/.config/nvim/lua/plugins/cle_plugins.lua
+++ vim/.config/nvim/lua/plugins/cle_plugins.lua
@@ -4,12 +4,15 @@ return {
 
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
+  { "sainnhe/gruvbox-material" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "gruvbox", -- alternative: gruvbox-material
+      gruvbox_material_background = "medium",
+      gruvbox_material_better_performance = 1,
     },
   },
 }
