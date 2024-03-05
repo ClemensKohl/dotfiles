fe9f0acc9637218d1d2254c953d5e3e8749adbde
Clemens Kohl <kohl.clemens@gmail.com>
2024-03-05 19:21:22 +0100

    upd

--- vim/.config/nvim/lua/plugins/cle_plugins.lua
+++ vim/.config/nvim/lua/plugins/cle_plugins.lua
@@ -507,6 +507,19 @@ return {
       -- these are examples, not defaults. Please see the readme
       vim.g.molten_image_provider = "image.nvim"
       vim.g.molten_output_win_max_height = 20
+      -- I find auto open annoying, keep in mind setting this option will require setting
+      -- a keybind for `:noautocmd MoltenEnterOutput` to open the output again
+      vim.g.molten_auto_open_output = true
+
+      -- optional, I like wrapping. works for virt text and the output window
+      vim.g.molten_wrap_output = true
+
+      -- Output as virtual text. Allows outputs to always be shown, works with images, but can
+      -- be buggy with longer images
+      vim.g.molten_virt_text_output = true
+
+      -- this will make it so the output shows up below the \`\`\` cell delimiter
+      vim.g.molten_virt_lines_off_by_1 = true
     end,
   },
 
@@ -558,8 +571,4 @@ return {
     -- Depending on your nvim distro or config you may need to make the loading not lazy
     -- lazy=false,
   },
-
-  -- {
-  --   "goerz/jupytext.vim",
-  -- },
 }
