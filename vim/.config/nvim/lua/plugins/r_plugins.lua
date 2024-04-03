570620cb51f9afbb65086d4b26ca7ff223b3a903
Clemens Kohl <kohl.clemens@gmail.com>
2024-04-03 22:45:25 +0200

    r fixes

--- vim/.config/nvim/lua/plugins/r_plugins.lua
+++ vim/.config/nvim/lua/plugins/r_plugins.lua
@@ -23,16 +23,27 @@ return {
   { "R-nvim/cmp-r" },
 
   -- Below is necessary for cmp-r
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = { "R-nvim/cmp-r" },
+  --   opts = function(_, opts)
+  --     ---@param opts cmp.ConfigSchema
+  --     local cmp = require("cmp")
+  --     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "cmp-r" } }))
+  --     require("cmp_r").setup({})
+  --   end,
+  -- },
+
+  -- This is a test, remove if it doesnt work.
   {
     "hrsh7th/nvim-cmp",
     dependencies = { "R-nvim/cmp-r" },
+    ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      ---@param opts cmp.ConfigSchema
-      local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "cmp-r" } }))
-      require("cmp_r").setup({})
+      table.insert(opts.sources, { name = "cmp_r" })
     end,
   },
+
 
   -- This is the config for cmp from the github page:
   -- {
