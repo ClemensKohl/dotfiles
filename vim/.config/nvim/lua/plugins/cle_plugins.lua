fb9ee8784d3f099813cd4640016b872eeed239fa
Clemens Kohl <kohl.clemens@gmail.com>
2024-04-17 19:57:18 +0200

    ltex upd

--- vim/.config/nvim/lua/plugins/cle_plugins.lua
+++ vim/.config/nvim/lua/plugins/cle_plugins.lua
@@ -44,10 +44,12 @@ return {
       },
       servers = {
         ltex = {
+          use_spellfile = false,
+          on_attach = on_attach,
+          capabilities = capabilities,
+          filetypes = { "latex", "tex", "bib", "markdown", "gitcommit", "text" },
           settings = {
             ltex = {
-              -- on_attach = on_attach,
-              -- capabilities = capabilities,
               disabledRules = {
                 ["en"] = { "MORFOLOGIK_RULE_EN" },
                 ["en-AU"] = { "MORFOLOGIK_RULE_EN_AU" },
@@ -64,10 +66,34 @@ return {
               --   ["en-US"] = DICT_words,
               --   ["en-GB"] = DICT_words,
               -- },
-              language = "en-US", -- "de-DE"
+              language = "en-US",
+              -- language = "de-DE",
               additionalRules = {
                 languageModel = "~/.local/share/ngrams/",
               },
+              dictionary = (function()
+                -- For dictionary, search for files in the runtime to have
+                -- and include them as externals the format for them is
+                -- dict/{LANG}.txt
+                --
+                -- Also add dict/default.txt to all of them
+                local files = {}
+                for _, file in ipairs(vim.api.nvim_get_runtime_file("dict/*", true)) do
+                  local lang = vim.fn.fnamemodify(file, ":t:r")
+                  local fullpath = vim.fs.normalize(file, ":p")
+                  files[lang] = { ":" .. fullpath }
+                end
+
+                if files.default then
+                  for lang, _ in pairs(files) do
+                    if lang ~= "default" then
+                      vim.list_extend(files[lang], files.default)
+                    end
+                  end
+                  files.default = nil
+                end
+                return files
+              end)(),
             },
           },
         },
@@ -353,5 +379,12 @@ return {
         },
       },
     },
+  },
+
+  -- Plugin improves neovim and ltex-ls interoperability.
+  -- If you want to use nvim-lspconfig
+  {
+    "vigoux/ltex-ls.nvim",
+    dependencies = "neovim/nvim-lspconfig",
   },
 }
