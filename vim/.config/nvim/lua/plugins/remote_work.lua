c297415a817e20a1fafc7395de4a63278e5c46d7
Clemens Kohl <kohl.clemens@gmail.com>
2024-04-28 16:56:37 +0200

    switched up nvim config a bit

---
+++ vim/.config/nvim/lua/plugins/remote_work.lua
@@ -0,0 +1,13 @@
+-- Helpful plugins for remote work.
+-- stylua: ignore
+if true then return {} end
+
+return {
+  -- Transfer files from and to remote servers.
+  {
+    "coffebar/transfer.nvim",
+    lazy = true,
+    cmd = { "TransferInit", "DiffRemote", "TransferUpload", "TransferDownload", "TransferDirDiff", "TransferRepeat" },
+    opts = {},
+  },
+}
