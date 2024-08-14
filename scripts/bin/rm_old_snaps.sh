876a7d77777716a5397ef6af44289c06cfb84384
Clemens Kohl <kohl.clemens@gmail.com>
2024-08-14 23:53:25 +0200

    .

--- scripts/bin/rm_old_snaps.sh
+++ scripts/bin/rm_old_snaps.sh
@@ -2,6 +2,11 @@
 # Removes old revisions of snaps
 # https://superuser.com/questions/1310825/how-to-remove-old-version-of-installed-snaps/1330590#1330590
 # CLOSE ALL SNAPS BEFORE RUNNING THIS
+
+# You might also want to limit how many revisions are kept.
+# To set it to two (needs to be between 2 and 20):
+# sudo snap set system refresh.retain=2
+#
 set -eu
 
 LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
