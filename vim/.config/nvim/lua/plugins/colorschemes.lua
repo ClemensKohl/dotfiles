987074aef838cb0a6806ec3b69dc3ba3b7ac68dc
Clemens Kohl <kohl.clemens@gmail.com>
2024-08-22 17:55:49 +0200

    .

--- vim/.config/nvim/lua/plugins/colorschemes.lua
+++ vim/.config/nvim/lua/plugins/colorschemes.lua
@@ -35,57 +35,10 @@ return {
       end,
     },
   },
-  -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   priority = 1000,
-  --   opts = {
-  --     flavour = "frappe", -- latte, frappe, macchiato, mocha
-  --     background = { -- :h background
-  --       light = "latte",
-  --       dark = "mocha",
-  --     },
-  --     term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
-  --     compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
-  --     transparent_background = false, -- disables setting the background color.
-  --     integrations = {
-  --       aerial = true,
-  --       alpha = true,
-  --       cmp = true,
-  --       dashboard = true,
-  --       flash = true,
-  --       gitsigns = true,
-  --       headlines = true,
-  --       illuminate = true,
-  --       indent_blankline = { enabled = true },
-  --       leap = true,
-  --       lsp_trouble = true,
-  --       mason = true,
-  --       markdown = true,
-  --       mini = true,
-  --       native_lsp = {
-  --         enabled = true,
-  --         underlines = {
-  --           errors = { "undercurl" },
-  --           hints = { "undercurl" },
-  --           warnings = { "undercurl" },
-  --           information = { "undercurl" },
-  --         },
-  --       },
-  --       navic = { enabled = true, custom_bg = "lualine" },
-  --       neotest = true,
-  --       neotree = true,
-  --       noice = true,
-  --       notify = true,
-  --       semantic_tokens = true,
-  --       telescope = true,
-  --       treesitter = true,
-  --       treesitter_context = true,
-  --       which_key = true,
-  --     },
-  --   },
-  -- },
-  --
+
+  -- Nightfox
+  { "EdenEast/nightfox.nvim" },
+
   -- Rose pine theme
   {
     "rose-pine/neovim",
