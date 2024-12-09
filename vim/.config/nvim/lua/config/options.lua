db4f51e7d093303f6c4ce37d2613409aef30717f
Clemens Kohl <kohl.clemens@gmail.com>
2024-12-09 18:51:02 +0100

    sioyek

--- vim/.config/nvim/lua/config/options.lua
+++ vim/.config/nvim/lua/config/options.lua
@@ -79,7 +79,10 @@ vim.g.Rout_more_colors = 1
 
 if vim.fn.executable("zathura") == 1 then
   vim.g.vimtex_view_method = "zathura"
+elseif vim.fn.executable("sioyek") == 1 then
+  vim.g.vimtex_view_method = "sioyek"
 end
+
 -- vim.g.vimtex_quickfix_ignore_filters = { "Underfull", "Overfull" }
 vim.g.vimtex_quickfix_open_on_warning = 0
 
