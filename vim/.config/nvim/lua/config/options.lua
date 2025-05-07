33248c9f43899b821ff58cc89397f0bf2dbe8b98
Clemens Kohl <kohl.clemens@gmail.com>
2025-05-08 00:26:22 +0200

    finally snippets!

--- vim/.config/nvim/lua/config/options.lua
+++ vim/.config/nvim/lua/config/options.lua
@@ -43,6 +43,10 @@ vim.g.lazyvim_picker = "snacks"
 -- Set to false to disable.
 vim.g.lazygit_config = true
 
+-- Set to `false` to prevent "non-lsp snippets"" from appearing inside completion windows
+-- Motivation: Less clutter in completion windows and a more direct usage of snippits
+vim.g.lazyvim_mini_snippets_in_completion = true
+
 -------------
 -- NEOVIDE --
 -------------
