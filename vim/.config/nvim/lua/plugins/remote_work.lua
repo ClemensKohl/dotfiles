b8fdce9d42c58c80c0a34305aa28f4b1ee4af50e
Clemens Kohl <kohl.clemens@gmail.com>
2025-05-26 00:18:30 +0200

    upd home

--- vim/.config/nvim/lua/plugins/remote_work.lua
+++ vim/.config/nvim/lua/plugins/remote_work.lua
@@ -3,14 +3,6 @@
 if true then return {} end
 
 return {
-  -- Transfer files from and to remote servers.
-  {
-    "coffebar/transfer.nvim",
-    lazy = true,
-    cmd = { "TransferInit", "DiffRemote", "TransferUpload", "TransferDownload", "TransferDirDiff", "TransferRepeat" },
-    opts = {},
-  },
-
   {
     "amitds1997/remote-nvim.nvim",
     version = "*", -- Pin to GitHub releases
