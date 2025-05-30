e408ac520243124303a7a2237240a971fcc1c5f0
Clemens Kohl <kohl.clemens@gmail.com>
2025-05-30 11:42:26 +0200

    explorer preview

--- vim/.config/nvim/lua/plugins/snacks_config.lua
+++ vim/.config/nvim/lua/plugins/snacks_config.lua
@@ -35,6 +35,33 @@ return {
       },
       image = { enabled = false },
       terminal = { enabled = true },
+      picker = {
+        layout = { preset = "ivy" },
+        sources = {
+          explorer = {
+            auto_close = false,
+            cycle = true,
+            layout = { preview = "main" },
+            -- layout = {
+            -- { preview = true },
+            -- layout = {
+            --   box = "horizontal",
+            --   width = 0.8,
+            --   height = 0.8,
+            --   {
+            --     box = "vertical",
+            --     border = "rounded",
+            --     title = "{source} {live} {flags}",
+            --     title_pos = "center",
+            --     { win = "input", height = 1, border = "bottom" },
+            --     { win = "list", border = "none" },
+            --   },
+            --   { win = "preview", border = "rounded", width = 0.7, title = "{preview}" },
+            -- },
+            -- },
+          },
+        },
+      },
     },
   },
 }
