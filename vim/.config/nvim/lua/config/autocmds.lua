45da51e9c0491528f50996056f4eaf8c636a3b78
Clemens Kohl <kohl.clemens@gmail.com>
2025-06-10 19:04:04 +0200

    work

--- vim/.config/nvim/lua/config/autocmds.lua
+++ vim/.config/nvim/lua/config/autocmds.lua
@@ -2,6 +2,8 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+vim.cmd("silent! Copilot disable")
+
 -- Disable autoformat for certain filetypes.
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "r", "sh" },
--- vim/.config/nvim/queries/markdown/textobjects.scm
+++ vim/.config/nvim/queries/markdown/textobjects.scm
@@ -1,14 +1,14 @@
-; (atx_heading
-;   heading_content: (_) @class.inner) @class.outer
-;
-; (setext_heading
-;   heading_content: (_) @class.inner) @class.outer
-;
-; (thematic_break) @class.outer
+(atx_heading
+  heading_content: (_) @class.inner) @class.outer
 
-; (fenced_code_block (code_fence_content) @block.inner) @block.outer
-;
-; [
-;   (paragraph)
-;   (list)
-; ] @block.outer
+(setext_heading
+  heading_content: (_) @class.inner) @class.outer
+
+(thematic_break) @class.outer
+
+(fenced_code_block (code_fence_content) @block.inner) @block.outer
+
+[
+  (paragraph)
+  (list)
+] @block.outer
