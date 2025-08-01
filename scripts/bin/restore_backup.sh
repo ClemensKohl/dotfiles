8f187241b0998ad38de8a787f338338efd62e586
Clemens Kohl <kohl.clemens@gmail.com>
2025-08-01 11:53:31 +0200

    .

--- scripts/bin/restore_backup.sh
+++ scripts/bin/restore_backup.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-DRIVE=/media/clemens/backup_drive
-SOURCE=$DRIVE/250131/
-TARGET=$HOME/
+# DRIVE=/media/clemens/backup_drive
+# SOURCE=$DRIVE/250131/
+# TARGET=$HOME/
+# MNT=/run/media/clemens/backup_drive/
 
-DEL=$HOME/.bu/Deleted
+SOURCE=/home/clemens/tmp/home/clemens
+TARGET=$HOME/
+MNT=/home/clemens/tmp/
+
 
 today=$(date +'%Y%m%d')
+time=$(date +'%H%M')
+DEL=$HOME/.bu/Deleted/${today}
 LOGDIR=$HOME/.bu/log/${today}
 
 mkdir -p $LOGDIR
-time=$(date +'%H%M')
 
-if mountpoint -q /media/clemens/backup_drive/; then
-  rsync -abv --delete --backup-dir=$DEL $SOURCE/Documents/ $TARGET/Documents/ &>$LOGDIR/${time}_Documents.log
+if mountpoint -q $MNT; then
+  # rsync -abv --delete --backup-dir=$DEL $SOURCE/Documents/ $TARGET/Documents/ &>$LOGDIR/${time}_Documents.log
+  rsync -abv --delete --dry-run --backup-dir=$DEL $SOURCE/Documents/ $TARGET/Documents/ &>$LOGDIR/${time}_Documents.log
+  # cp -r $HOME/Desktop/Steuerklärung $HOME/Documents/Clemens/Finanzen/Steuern/Steuererklärung/2024/Steuerklärung
+
 else
   echo "\n/media/clemens/backup_drive/ is not not mounted"
 fi
