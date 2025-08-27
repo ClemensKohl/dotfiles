3e28abe817c14e0dc3de868a0e1820cdad24205b
Clemens Kohl <kohl.clemens@gmail.com>
2025-08-27 14:07:38 +0200

    upd work

--- vim/.config/nvim/lua/plugins/writing.lua
+++ vim/.config/nvim/lua/plugins/writing.lua
@@ -1,98 +1,51 @@
 -- Plugins for writing.
 -- stylua: ignore
-if true then return {} end
+-- if true then return {} end
 
 return {
+  -- Ltex Language Server. Ltex-ls does not seem to be maintained.
+  {
+    "ltex-plus/ltex-ls-plus",
+    dependencies = "neovim/nvim-lspconfig",
+  },
 
-  -- Plugin improves neovim and ltex-ls interoperability.
-  -- If you want to use nvim-lspconfig
+  -- Enable Code Actions for Ltex-LS
   -- {
-  --   "vigoux/ltex-ls.nvim",
-  --   dependencies = "neovim/nvim-lspconfig",
+  --   "barreiroleo/ltex_extra.nvim",
+  --   ft = { "markdown", "tex" },
+  --   dependencies = { "neovim/nvim-lspconfig" },
+  --   -- yes, you can use the opts field, just I'm showing the setup explicitly
+  --   opts = {
+  --       load_langs = { "en-US" }, -- en-US as default
+  --       init_check = true,
+  --       path = vim.fn.expand("~") .. "/.config/nvim/spell/ltex", -- project root or current working directory
+  --   },
+  --   -- config = function()
+  --   --   require("ltex_extra").setup {
+  --   --     -- table <string> : languages for witch dictionaries will be loaded, e.g. { "es-AR", "en-US" }
+  --   --     -- https://valentjn.github.io/ltex/supported-languages.html#natural-languages
+  --   --     load_langs = { "en-US" }, -- en-US as default
+  --   --     -- boolean : whether to load dictionaries on startup
+  --   --     init_check = true,
+  --   --     -- string : relative or absolute path to store dictionaries
+  --   --     -- e.g. subfolder in the project root or the current working directory: ".ltex"
+  --   --     -- e.g. shared files for all projects:  vim.fn.expand("~") .. "/.local/share/ltex"
+  --   --     path = vim.fn.expand("~") .. "/.config/nvim/spell/ltex", -- project root or current working directory
+  --   --     -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
+  --   --     log_level = "none",
+  --   --     -- table : configurations of the ltex language server.
+  --   --     -- Only if you are calling the server from ltex_extra
+  --   --     server_opts = nil,
+  --   --     -- server_opts = {
+  --   --     --   -- capabilities = your_capabilities,
+  --   --     --   on_attach = function(client, bufnr)
+  --   --     --     -- your on_attach process
+  --   --     --   end,
+  --   --     --   settings = {
+  --   --     --     -- ltex = { your settings }
+  --   --     --   }
+  --   --     -- },
+  --   --   }
+  --   -- end
   -- },
-
-  -- Telescope Thesaurus picker
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "rafi/telescope-thesaurus.nvim" },
-  },
-
-  -- Mode for focused writing.
-  {
-    "folke/zen-mode.nvim",
-    opts = {
-      window = {
-        backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-        -- height and width can be:
-        -- * an absolute number of cells when > 1
-        -- * a percentage of the width / height of the editor when <= 1
-        -- * a function that returns the width or the height
-        width = 120, -- width of the Zen window
-        height = 1, -- height of the Zen window
-        -- by default, no options are changed for the Zen window
-        -- uncomment any of the options below, or add other vim.wo options you want to apply
-        options = {
-          -- signcolumn = "no", -- disable signcolumn
-          -- number = false, -- disable number column
-          -- relativenumber = false, -- disable relative numbers
-          -- cursorline = false, -- disable cursorline
-          -- cursorcolumn = false, -- disable cursor column
-          -- foldcolumn = "0", -- disable fold column
-          -- list = false, -- disable whitespace characters
-        },
-      },
-      plugins = {
-        -- disable some global vim options (vim.o...)
-        -- comment the lines to not apply the options
-        options = {
-          enabled = true,
-          ruler = false, -- disables the ruler text in the cmd line area
-          showcmd = false, -- disables the command in the last line of the screen
-          -- you may turn on/off statusline in zen mode by setting 'laststatus'
-          -- statusline will be shown only if 'laststatus' == 3
-          laststatus = 0, -- turn off the statusline in zen mode
-        },
-        twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
-        gitsigns = { enabled = false }, -- disables git signs
-        tmux = { enabled = false }, -- disables the tmux statusline
-        -- this will change the font size on kitty when in zen mode
-        -- to make this work, you need to set the following kitty options:
-        -- - allow_remote_control socket-only
-        -- - listen_on unix:/tmp/kitty
-        kitty = {
-          enabled = true,
-          font = "+2", -- font size increment
-        },
-      },
-      -- callback where you can add custom code when the Zen window opens
-      on_open = function(win) end,
-      -- callback where you can add custom code when the Zen window closes
-      on_close = function() end,
-    },
-  },
-
-  -- dims inactive code/text. pairs well with zenmode.
-  {
-    "folke/twilight.nvim",
-    opts = {
-      dimming = {
-        alpha = 0.25, -- amount of dimming
-        -- we try to get the foreground from the highlight groups or fallback color
-        color = { "Normal", "#ffffff" },
-        term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
-        inactive = true, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
-      },
-      context = 10, -- amount of lines we will try to show around the current line
-      treesitter = true, -- use treesitter when available for the filetype
-      -- treesitter is used to automatically expand the visible text,
-      -- but you can further control the types of nodes that should always be fully expanded
-      expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
-        "function",
-        "method",
-        "table",
-        "if_statement",
-      },
-      exclude = {}, -- exclude these filetypes
-    },
-  },
 }
